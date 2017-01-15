bee272df28cc3e43ec88007dcc96c82a59d3e393
Yoshiki Ikegaya <mr.ikegaya@gmail.com>
2017-01-15 19:50:17 +0900

    first commit

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour {
+
+	// Use this for initialization
+	public void GameStart () {
+		SceneManager.LoadScene ("Main");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speedX;
+	public float speedZ;
+
+	public GameObject bullet;
+	float bulletInterval;
+
+	public GameObject enemy;
+	float enemyInterval;
+
+	public GameObject explosion;
+
+	Slider slider;
+	int playerLife;
+
+	// Use this for initialization
+	void Start () {
+		bulletInterval = 0.0f;
+		enemyInterval = 0.0f;
+		playerLife = 3;
+		slider = GameObject.Find ("Slider").GetComponent<Slider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float vertical = Input.GetAxis ("Vertical");
+		float horizontal = Input.GetAxis ("Horizontal");
+
+		if (Input.GetKey ("up")) {
+			MoveToUp (vertical);
+		}
+		if (Input.GetKey ("right")) {
+			MoveToRight (horizontal);
+		}
+		if (Input.GetKey ("left")) {
+			MoveToLeft (horizontal);
+		}
+		if (Input.GetKey ("down")) {
+			MoveToDown (vertical);
+		}
+
+		bulletInterval += Time.deltaTime;
+		if (Input.GetKey ("space")) {
+			if (bulletInterval >= 0.2f) {
+				GenerateBullet ();
+			}
+		}
+
+		enemyInterval += Time.deltaTime;
+		if (enemyInterval >= 5.0f) {
+			GenerateEnemy ();
+		}
+	}
+
+	void MoveToUp(float vertical){
+		transform.Translate (0, 0, vertical * speedZ);
+	}
+
+	void MoveToRight(float horizontal){
+		transform.Translate (horizontal * speedX, 0, 0);
+	}
+
+	void MoveToLeft(float horizontal){
+		transform.Translate (horizontal * speedX, 0, 0);
+	}
+
+	void MoveToDown(float vertical){
+		transform.Translate (0, 0, vertical * speedZ);
+	}
+
+	void GenerateBullet(){
+		bulletInterval = 0.0f;
+		Instantiate (bullet, transform.position, Quaternion.identity);
+	}
+
+	void GenerateEnemy(){
+		Quaternion q = Quaternion.Euler (0, 180, 0);
+		enemyInterval = 0.0f;
+		Instantiate (
+			enemy,
+			new Vector3 (Random.Range (-100, 100), transform.position.y, transform.position.z + 200),
+			q
+		);
+		Instantiate(
+			enemy,
+			new Vector3(transform.position.x, transform.position.y, transform.position.z + 200),
+			q
+		);
+	}
+
+	void OnTriggerEnter(Collider coll) {
+		if (coll.gameObject.tag == "EnemyBullet") {
+			playerLife--;
+			slider.value = playerLife;
+			Instantiate (
+				explosion,
+				new Vector3 (transform.position.x, transform.position.y, transform.position.z),
+				Quaternion.identity
+			);
+			Destroy (coll.gameObject);
+			if (playerLife <= 0) {
+				Destroy (this.gameObject);
+
+				ScoreController obj = GameObject.Find ("Main Camera").GetComponent<ScoreController> ();
+				obj.SaveHighScore ();
+			}
+		}
+	}
+}
